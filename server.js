49eb1aefe0b1bda1876df53b82aaaccb51069b59
Rohit Singh <rohit.singh.33521@gmail.com>
2024-10-13 16:01:57 +0530

    add health check

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
